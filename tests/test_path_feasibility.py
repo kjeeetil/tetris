4ba847501263dd4844ea3dc40106d58daeeaaf0f
kjeeetil <kjetil.mo@gmail.com>
2025-09-13 12:56:47 +0200

    Account for gravity during horizontal translation

--- tests/test_path_feasibility.py
+++ tests/test_path_feasibility.py
@@ -8,7 +8,14 @@ from tetris.placement_env import _enumerate_placements
 def test_unreachable_columns_excluded():
     board = Board()
     # block between spawn column (3) and column 0
-    board.grid[0][2] = 1
-    actions = _enumerate_placements(board, TetrominoType.I)
+    board.grid[1][2] = 1
+    actions = _enumerate_placements(board, TetrominoType.I, level=0)
     cols = [a.column for a in actions]
     assert all(c >= 3 for c in cols), cols
+
+
+def test_level_29_no_horizontal_movement():
+    board = Board()
+    actions = _enumerate_placements(board, TetrominoType.I, level=29)
+    cols = {a.column for a in actions}
+    assert cols == {board.width // 2 - 2}
